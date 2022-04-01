29cc8ec697ab019ec479117178d4b2eb293b0845
wyl350 <wyl350@126.com>
2022-04-01 15:08:20 +0800

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,15 +1,22 @@
+import Layout from '../../components/layout'
+import Script from 'next/script'
+import Head from 'next/head'
 import Link from 'next/link'
 
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+
+            <Head>
+                <title>wyl350-first-posts</title>
+            </Head>
             <h1>First Post</h1>
             <h2>
                 <Link href='/'>
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
---
+++ pages/profile.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+export default () => {
+    return <>
+
+        <Image
+            src='/images/profile.jpg'
+            height={144}
+            width={144}
+            alt='wyl350'
+        />
+    </>
+}
