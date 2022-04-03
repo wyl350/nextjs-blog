c0b55567b0d7ef043563510198132f4276b1f7f5
wyl350 <wyl350@126.com>
2022-04-03 16:30:22 +0800

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import Link from 'next/link'
+import Date from '../components/date'
+
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
@@ -11,14 +14,6 @@ export async function getStaticProps() {
     }
   }
 }
-
-// export async function getServerSideProps() {
-//   return {
-//     props: {
-//       name: 'wyl350',
-//     }
-//   }
-// }
 
 export default function Home({ allPostData, name }) {
   return (
@@ -38,11 +33,13 @@ export default function Home({ allPostData, name }) {
         <ul className={utilStyles.list}>
           {
             allPostData.map(({ id, date, title }) => <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>)
           }
         </ul>
